cca4e867617f2d984bb6f307bd0fb6135a3ad28a
Sergey Klupin <yetibrain@yandex.ru>
2018-06-24 19:36:13 +0300

    Добавлен метод для получения маркера доступа и информации о пользователе.

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const {URL, URLSearchParams} = require('url');
 const getTimestamp = require('./lib/timestamp');
 const getSigner = require('./lib/signer');
 const uuid = require('uuid/v4');
+const querystring = require('querystring');
+const request = require('request-promise');
+const jwtDecode = require('jwt-decode');
+const prns = require('./lib/loader');
 
 const defaultConfig = {
   esiaUrl: 'https://esia.gosuslugi.ru',
   authPath: '/aas/oauth2/ac',
   markerPath: '/aas/oauth2/te',
+  dataPath: '/rs/prns',
   scope: 'openid',
 };
 
@@ -15,6 +20,7 @@ const requiredFields = [
   'esiaUrl',
   'authPath',
   'markerPath',
+  'dataPath',
   'scope',
   'clientId',
   'redirectUri',
@@ -23,6 +29,7 @@ const requiredFields = [
 ];
 
 /**
+ * @function
  * @param {Object} config Конфиг подключения к ЕСИА.
  * @prop {String} esiaUrl Урл портала ЕСИА.
  * (по умолчанию 'https://esia.gosuslugi.ru')
@@ -73,24 +80,27 @@ module.exports = (config) => {
     redirectUri,
     scope,
     authPath,
+    markerPath,
+    dataPath,
     esiaUrl,
     certificate,
     key,
   } = _conf;
 
   const authUrl = new URL(authPath, esiaUrl);
+  const markerUrl = new URL(markerPath, esiaUrl);
+  const dataUrl = new URL(dataPath, esiaUrl);
   const sign = getSigner({certificate, key});
 
   return {
     /**
-     * Метод возвращает данные для
-     * авторизации через портал ЕСИА
-     *
+     * Метод возвращает данные для авторизации.
+     * @function getAuth
      * @return {Object}
      * @prop {String} url Ссылка для авторизации в ЕСИА.
      * @prop {Object} params Параметры, использованные при построении ссылки.
      */
-    createAuth() {
+    getAuth() {
       const timestamp = getTimestamp();
       const state = uuid();
       const clientSecret = sign([scope, timestamp, clientId, state].join(''));
@@ -110,6 +120,85 @@ module.exports = (config) => {
         url: `${authUrl}?${authQuery}`,
         params,
       };
+    },
+
+    /**
+     * Метод получения маркера доступа и информации о пользователе
+     * @function getAccess
+     * @param {String} code Авторизационны код, возвращаемый на redirect_uri.
+     * @param {String[]} dataPathList Массив путей для получения информации
+     * о пользователе. Например ['/', '/docs'].
+     * (по умлолчанию используется ['/'] - корневой путь.
+     * В итоге, будет загружена информиация по пути 'https://esia.gosuslugi.ru/rs/prns/100321/')
+     * Если передать null, то информация о пользователе запрашиваться не будет.
+     *
+     * @return {Promise<Object>} Возвращает промис, который резолвится в объект
+     * с ответом на запрос маркера.
+     * @prop {Object} marker Объект маркера доступа. Содержит два поля:
+     * 1. response - ответ от есиа при получении маркера
+     * 2. decodedAccessToken - jwt декодированное поле access_token из response
+     *
+     * @prop {Object} data Данные о пользователе есиа в том же порядке что и
+     * пути для запроса данных, переданные в dataPathList
+     */
+    getAccess(code, dataPathList) {
+      const timestamp = getTimestamp();
+      const state = uuid();
+      const clientSecret = sign([scope, timestamp, clientId, state].join(''));
+
+      if (!code) {
+        return Promise.reject(
+          new Error('Code is required to get access marker')
+        );
+      }
+
+      return request({
+        method: 'POST',
+        uri: markerUrl,
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: querystring.stringify({
+          client_id: clientId,
+          client_secret: clientSecret,
+          redirect_uri: redirectUri,
+          code,
+          state,
+          scope,
+          timestamp,
+          grant_type: 'authorization_code',
+          token_type: 'Bearer',
+        }),
+      })
+        .then((stringlyResponse) => {
+          const response = JSON.parse(stringlyResponse);
+          const decodedAT = jwtDecode(response.access_token);
+
+          if (!Array.isArray(dataPathList)) {
+            dataPathList = dataPathList === undefined ? ['/'] : [];
+          }
+
+          return Promise.all(
+            dataPathList.map((path) => {
+              return prns({
+                uri: `${dataUrl}/${decodedAT['urn:esia:sbj_id']}${path}`,
+                accessToken: response.access_token,
+              });
+            })
+          )
+            .then((data) => ({
+              marker: {
+                response,
+                decodedAccessToken: decodedAT,
+              },
+              data,
+            }));
+        })
+        .catch((err) => {
+          throw new Error(
+            'Error while getting marker or handling response. ' + err.message
+          );
+        });
     },
   };
 };
